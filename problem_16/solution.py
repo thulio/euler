76b8ca8306506a95c56c55b92ca7ac370d958cb3
Thúlio Costa <thulio@thuliocosta.com>
2013-04-23 17:53:59 -0300

    Solution 16

---
+++ problem_16/solution.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+from decimal import Decimal
+import functools
+import math
+
+
+def sum_of_digits(number):
+    list_of_digits = [int(digit) for digit in str(Decimal(number))]
+    return functools.reduce(lambda x, y: x + y, list_of_digits)
+
+
+if __name__ == "__main__":
+    print(sum_of_digits(2**15))
+    assert 26 == sum_of_digits(2**15)
+    number = Decimal(math.pow(2, 1000))
+    print("{}".format(sum_of_digits(number)))
